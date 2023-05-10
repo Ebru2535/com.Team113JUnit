e9e29bcf111f379b362fbc45e5c2c2364cb459b6
Ebru2535 <ebrusavas.2015@gmail.com>
2023-05-10 08:40:11 +0300

    10/05/2023

--- src/test/java/day06_JUnitFramework/C05_Assertions.java
+++ src/test/java/day06_JUnitFramework/C05_Assertions.java
@@ -25,9 +25,10 @@ public class C05_Assertions {
     public void test01(){
         // ○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
         mahserin4Atlisi();
-        String expectedUrl="https://www.bestbuy.com";
+        String expectedUrl="https://www.bestbuy.com/";
         String actualUrl= driver.getCurrentUrl(); // https://www.bestbuy.com/
         Assert.assertEquals(expectedUrl,actualUrl);
+        driver.close();
     }
     @Test
     public void test02(){
@@ -35,5 +36,6 @@ public class C05_Assertions {
         String unExpectedIcerik = "Rest";
         String actualTitle= driver.getTitle();
         Assert.assertFalse(actualTitle.contains(unExpectedIcerik));
+        driver.close();
     }
 }
